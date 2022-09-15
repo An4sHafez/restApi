7fc806be10d4b46421058553362667316a8a1179
An4sHafez <100023349+An4sHafez@users.noreply.github.com>
2022-09-15 15:59:45 +0300

    Delete Cats/catInfo directory

--- Cats/catInfo/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Cat(models.Model):
-  name = models.CharField(max_length=20)
-  breeds= models.CharField(max_length=20)
-  color= models.CharField(max_length=20,null=True)
-  #img = models.ImageField()
-  
--- Cats/catInfo/serializers.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import field
-import imp
-from pyexpat import model
-from rest_framework import serializers
-from .models import Cat
-class CatsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Cat
-        fields='__all__'
